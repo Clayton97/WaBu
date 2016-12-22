683c8fe66694825018bb94c66b519edfc54c5442
Clayton97 <cps5aa@virginia.edu>
2016-12-22 12:56:12 -0500

    Actual Files for Working Maps
    These are the actual files for the maps

---
+++ WaBu/WaBu/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  WaBu
+//
+//  Created by Clayton Smith on 12/4/16.
+//  Copyright © 2016 Clayton Smith. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    @IBOutlet weak var mapView: MKMapView!
+
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.locationManager.delegate = self
+        
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        self.locationManager.startUpdatingLocation()
+        
+        self.mapView.showsUserLocation = true
+        
+        self.mapView.delegate = self;
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //Try to make the current location the starting point
+    //Mark: Location Delegate Methods
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last
+        
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.latitude)
+        
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+        
+        self.mapView.setRegion(region, animated: true)
+        
+        //[self.mapview setUserTrackingMode(MKUserTrackingModeFollow animated:YES)]
+        
+        self.locationManager.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print("Errors: " + error.localizedDescription)
+    }
+    
+    
+}
+
